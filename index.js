db4cb474932b2f1225799db794f196aeef7395c8
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-08-25 21:14:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+let someValue;
+
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let message;
+  if(someValue <= 400) {
+    message = 'This one is on me!';
+  } else if (someValue > 2000 && someValue <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else {
+    message = 'No can do.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+let city;
+function ternaryCheckCity(city){
   // Write your code here!
+  let message;
+
+  city === 'NYC' ? (message = 'Ok, sounds good.') : (message = 'No go.');
+  return message;
 }
 
-function switchOnCharmFromTip(){
+let tip;
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+
+  switch(tip){
+    case "generous": message = "Thank you so much.";
+      break;
+    case "not as generous": message = "Thank you.";
+      break;
+    default: message = "Bye.";
+  }
+  return message;
 }
