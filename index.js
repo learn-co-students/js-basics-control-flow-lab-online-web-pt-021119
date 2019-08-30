23021625365aafe18023d1a4346b8076cf340258
lieba <liebahinda@gmail.com>
2019-08-29 21:41:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  let response;
+  if (distance <= 400) {
+    response = 'This one is on me!'
+  } else if (distance > 2000) {
+    if (distance > 2500) {
+      response = "No can do."
+    } else {
+      response = "I will gladly take your thirty bucks."
+    }
+  };
+  return response
+};
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const response = city === 'NYC' ? "Ok, sounds good." : "No go."
+
+  return response;
+};
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip) {
+    case "generous":
+      response = 'Thank you so much.'
+      break;
+
+    case "not as generous":
+      response = "Thank you."
+      break;
+
+    default:
+    response = "Bye."
+  };
+  return response;
 }
