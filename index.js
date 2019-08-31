39be21a1790036b203e7d7f775252c99eff12a73
Tracy Ngo <tngo1206@gmail.com>
2019-08-31 11:01:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let answer
+  if (feet <= 400){
+    answer = 'This one is on me!';
+  }
+  else if (feet >= 2000 && feet < 2500){
+    answer = 'I will gladly take your thirty bucks.';
+  }
+  else if (feet >= 2500){
+    answer = 'No can do.';
+  }
+  return answer
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  let answer = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return answer
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    default:
+      return 'Bye.'
+      break;
+    }
+  }
